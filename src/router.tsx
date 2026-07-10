235356505980f9214e04efa481a90361df0c216d
yashvardhan7319 <jyash7319@gmail.com>
2026-07-10 14:50:12 +0530

    fix: Revert to browser history to fix anchor links

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter, createHashHistory } from "@tanstack/react-router";
+import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-
-const hashHistory = createHashHistory();
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
-    history: hashHistory,
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
